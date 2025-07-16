c388ec853aa531292736b9e2851fd8928a1b0623
Kristin <kristindyulgeryan@abv.bg>
2025-07-16 16:56:47 +0300

    implement getMessages controller to retrieve conversation between two users

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,6 @@
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
+
 
 export const getUsersForSidebar = async(req, res) =>{
     try {
@@ -10,5 +12,24 @@ export const getUsersForSidebar = async(req, res) =>{
         console.log("Error in getUsersForSidebar: ", error.message);
         res.status(500).json({error: "Internal server error"})
         
+    }
+}
+
+export const getMessages = async (req, res) => {
+    try {
+        const {id:userToChatId} = req.params
+        const myId = req.user._id
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: myId }
+            ]
+        })
+
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log("Error in getUsersForSidebar: ", error.message);
+        res.status(500).json({error: "Internal server error"})
     }
 }
