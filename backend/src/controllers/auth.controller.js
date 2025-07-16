438622d44f9d571aaa81352351b6cd68586b43e8
Kristin <kristindyulgeryan@abv.bg>
2025-07-16 11:23:09 +0300

    implement logout controller to clear JWT cookie

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -79,4 +79,12 @@ export const login = async (req, res) => {
 
 
 
-export const logout = (req, res) => res.send("logout route");
+export const logout = (req, res) => {
+  try {
+    res.cookie("jwt", "", {maxAge: 0})
+    res.status(200).json({message: "Logged out successfully"})
+  } catch (error) {
+    console.log("Error logout controller", error.message);
+    res.status(500).json({message: "Internal Server Error"})
+  }
+};
