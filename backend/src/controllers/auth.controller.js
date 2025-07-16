b8ef95971d9699b1805e44705e468af0faba8fb5
Kristin <kristindyulgeryan@abv.bg>
2025-07-16 15:33:01 +0300

    add checkAuth controller to return authenticated user data

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -110,3 +110,12 @@ const updatedUser = await User.findByIdAndUpdate(userId, {
   res.status(500).json({message: "Internal Server Error"})
 }
 }
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user)
+  } catch (error) {
+    console.log("Error in check controller: ", error.message);
+  res.status(500).json({message: "Internal Server Error"})
+  }
+}
