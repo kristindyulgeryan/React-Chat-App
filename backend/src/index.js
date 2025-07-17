e7a775f868838c3e2d07f82c4f8e4f30370f3f99
Kristin <kristindyulgeryan@abv.bg>
2025-07-17 13:30:06 +0300

    install and configure CORS for frontend connection with credentials support

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 
@@ -13,10 +14,17 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes)
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
