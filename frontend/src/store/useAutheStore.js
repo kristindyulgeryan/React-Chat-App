3dd6362ec09c908215737e2d60402d5ca21d5d1a
Kristin <kristindyulgeryan@abv.bg>
2025-07-17 13:32:00 +0300

    install axios for HTTP requests and zustand for state management

---
+++ frontend/src/store/useAutheStore.js
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigninUp: false,
+  isLoggingIn: false,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in checkAuth:", error);
+
+      set({ authuser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
