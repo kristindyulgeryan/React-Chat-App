8b8e371dac84b02d431f2ea42d73eb448bdfc9ee
Kristin <kristindyulgeryan@abv.bg>
2025-07-23 16:13:11 +0300

    Filter incoming messages to only show those from the selected user

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -54,8 +54,10 @@ export const useChatStore = create((set, get) => ({
 
     const socket = useAuthStore.getState().socket;
 
-    // todo: optimize this one later
     socket.on("newMessage", (newMessage) => {
+      const isMessageSentFromSelectedUser =
+        newMessage.senderId == selectedUser._id;
+      if (!isMessageSentFromSelectedUser) return;
       set({ messages: [...get().messages, newMessage] });
     });
   },
@@ -65,6 +67,5 @@ export const useChatStore = create((set, get) => ({
     socket.off("newMessage");
   },
 
-  // todo: optimize for later
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 }));
